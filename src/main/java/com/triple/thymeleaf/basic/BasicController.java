64549ee7e583660babeeaabbcfffc3c9a59b261b
윤경 <57247474+YoonkyungH@users.noreply.github.com>
2022-08-01 16:34:46 +0900

    Thymeleaf

--- src/main/java/com/triple/thymeleaf/basic/BasicController.java
+++ src/main/java/com/triple/thymeleaf/basic/BasicController.java
@@ -1,5 +1,6 @@
 package com.triple.thymeleaf.basic;
 
+import com.triple.domain.User2;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/basic")
@@ -27,9 +30,35 @@ public class BasicController {
         model.addAttribute("param1", "data1");
         model.addAttribute("param2", "data2");
 
+        // 리터럴(소스코드상 고정된 값) 사용시 '' 또는 ||로 감싸 사용
         model.addAttribute("data2", "Spring");
 
+        // 연산
+        model.addAttribute("nullData", null);
+
+        // 속성값 설정
+        model.addAttribute("isChecked", true);
+        model.addAttribute("isNotChecked", false);
+
+        // 반복, 조건부 평가, 블록
+        addUsers(model);
+
+        // 자바스크립트 인라인
+        model.addAttribute("user", new User2("userA", 10));
+        addUsers(model);
+
         return "basic/basic";
+    }
+
+    // 반복
+    private void addUsers(Model model) {
+        List<User2> list = new ArrayList<>();
+
+        list.add(new User2("userA", 10));
+        list.add(new User2("userB", 20));
+        list.add(new User2("userC", 30));
+
+        model.addAttribute("users", list);
     }
 
 }
