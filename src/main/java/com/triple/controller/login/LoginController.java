8496c0b08c04ac60d328fcf13fe245e1deb00964
윤경 <57247474+YoonkyungH@users.noreply.github.com>
2022-08-17 21:19:01 +0900

    로그인(redirect)

--- src/main/java/com/triple/controller/login/LoginController.java
+++ src/main/java/com/triple/controller/login/LoginController.java
@@ -11,6 +11,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -99,8 +100,50 @@ public class LoginController {
     /**
      * version 3
      */
+//    @PostMapping("/login")
+//    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request) {
+//        if (bindingResult.hasErrors()) {
+//            return "login/loginForm";
+//        }
+//
+//        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+//        log.info("login? {}", loginMember);
+//
+//        if (loginMember == null) {
+//            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+//            return "login/loginForm";
+//        }
+//
+//        // 로그인 성공 처리
+//        // 세션이 있으면 있는 세션 반환, 없으면 신규 세션 생성
+//        HttpSession session = request.getSession();
+//        // 세션에 로그인 회원 정보 보관
+//        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+//
+//        return "redirect:/";
+//    }
+//
+//    @PostMapping("/logout")
+//    public String logout(HttpServletRequest request) {
+//        // 세션 삭제
+//        HttpSession session = request.getSession(false);
+//        if (session != null) {
+//            session.invalidate();
+//        }
+//
+//        return "redirect:/";
+//    }
+
+    /**
+     * version 4
+     * 로그인 이후 redirect 처리
+     */
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request) {
+    public String loginV4(
+            @Valid @ModelAttribute LoginForm form, BindingResult bindingResult,
+            @RequestParam(defaultValue = "/") String redirectURL,
+            HttpServletRequest request)
+    {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
         }
@@ -113,25 +156,17 @@ public class LoginController {
             return "login/loginForm";
         }
 
-        // 로그인 성공 처리
-        // 세션이 있으면 있는 세션 반환, 없으면 신규 세션 생성
+        //로그인 성공 처리
+
+        //세션이 있으면 있는 세션 반환, 없으면 신규 세션 생성
         HttpSession session = request.getSession();
-        // 세션에 로그인 회원 정보 보관
+        //세션에 로그인 회원 정보 보관
         session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
 
-        return "redirect:/";
+        //redirectURL 적용
+        return "redirect:" + redirectURL;
     }
 
-    @PostMapping("/logout")
-    public String logout(HttpServletRequest request) {
-        // 세션 삭제
-        HttpSession session = request.getSession(false);
-        if (session != null) {
-            session.invalidate();
-        }
-
-        return "redirect:/";
-    }
 
     /**
      * 서버에서 해당 쿠키의 종료 날짜를 0으로 지정 -> 해당 쿠키 즉시 종료(시간을 예전 시간으로 돌려 쿠키 삭제)
